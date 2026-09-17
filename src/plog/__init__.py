e543f359347fa6ee2aaec39edf962b9d32237b3b
Jackal <thatonestupidwhiteboi@gmail.com>
2026-09-16 21:51:43 -0600

    Rename plog to synlog in __init__.py

--- src/plog/__init__.py
+++ src/plog/__init__.py
@@ -1,7 +1,7 @@
 """
-plog - Pretty colored Python logger with in-memory record buffering and JSON export.
+synlog - Pretty colored Python logger with in-memory record buffering and JSON export.
 """
-from .plog import plog, C, FMTTIMES, PLogFormatter, RBH
+from .synlog import plog, C, FMTTIMES, PLogFormatter, RBH
 __version__ = "1.1.0"
 __author__ = "J4ck3LSyN"
 __all__ = ["plog", "C", "FMTTIMES", "PLogFormatter", "RBH"]
