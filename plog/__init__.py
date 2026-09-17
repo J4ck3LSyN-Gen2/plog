d44ff2485c78b3fbc00877fd79c3d43bfd3dae6b
Jackal <thatonestupidwhiteboi@gmail.com>
2026-09-16 21:35:49 -0600

    Delete plog directory

--- plog/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-plog - Pretty colored Python logger with in-memory record buffering and JSON export.
-"""
-from .plog import plog, C, FMTTIMES, PLogFormatter, RBH
-__version__ = "1.1.0"
-__author__ = "J4ck3LSyN"
-__all__ = ["plog", "C", "FMTTIMES", "PLogFormatter", "RBH"]
